1a97f3870918545ac951eccda538c289d0d3934e
Lucas Formiga <lucasformiga@gmail.com>
2018-08-29 13:31:56 -0300

    Handling Database disconnection.

--- config/mysql.js
+++ config/mysql.js
@@ -8,6 +8,21 @@ var mysql           = require('mysql');
 var connectString   = {host: 'us-cdbr-iron-east-01.cleardb.net', user: 'ba0879772025da', password: '7851ecf0', database: 'heroku_02cbd90d63f0ef5'};
 var db              = mysql.createConnection(connectString);
 
+function handleDisconnect(cnx) {
+    cnx.on('error', function(err) {
+        if (!err.fatal) return;
+        if (err.code !== 'PROTOCOL_CONNECTION_LOST') throw err;
+
+        console.log('> Re-connecting lost main MySQL connection: ' + err.stack);
+
+        cnx = mysql.createConnection(cnx.config);
+        handleDisconnect(cnx);
+        cnx.connect();
+    });
+};
+
+handleDisconnect(db);
+
 module.exports = () => {
 	return db;
 }
